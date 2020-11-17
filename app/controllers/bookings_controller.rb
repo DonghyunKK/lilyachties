cfae6a0725467f2603ffcc07af80c93c8f6f5a52
Liam Jackson <liam.jackson@ldsn.co>
2020-11-17 11:21:37 +0000

    Created bookings controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,20 @@
+class BookingsController < ApplicationController
+  def create
+    @booking = Booking.new(booking_params)
+    # we need `yacht_id` to associate booking with corresponding yacht
+    @yacht = Yacht.find(params[:yacht_id])
+    @booking.yacht = @yacht
+    @booking.user = current_user
+    if @booking.save # ensures validations pass
+      redirect_to dashboard_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :accepted)
+  end
+end

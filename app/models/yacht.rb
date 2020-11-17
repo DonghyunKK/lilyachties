3b369f3357d444efc90159d640c4f72c3d5a7bf2
donghyun kim <donghyunkim524@gmail.com>
2020-11-17 18:01:50 +0000

    add validation and change input field

--- app/models/yacht.rb
+++ app/models/yacht.rb
@@ -1,4 +1,10 @@
 class Yacht < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  validates :title, presence: true, uniqueness: true
+  # validates :build, presence: true
+  # validates :coordinates, presence: true
+  # validates :description, length: { minimum: 20 }
+  # validates :year, numericality: { only_integer: true }
+  # validates :weekly_price, numericality: { only_integer: true }
 end

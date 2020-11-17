b83e24ed443fed8f851e3c40963ab14115413d78
Charles Crane <charlie.crane46@gmail.com>
2020-11-17 12:13:01 +0000

    Fix issue with yacht create

--- app/controllers/yachts_controller.rb
+++ app/controllers/yachts_controller.rb
@@ -8,13 +8,18 @@ class YachtsController < ApplicationController
   end
 
   def show
-    id = params[:id].to_i
+    id = params[:id]
     @yacht = Yacht.find(id)
   end
 
   def create
-    @yacht = Yacht.create(yacht_params)
-    redirect_to yacht_path(@yacht.id)
+    @yacht = Yacht.new(yacht_params)
+    @yacht.save
+    if @yacht.save
+      redirect_to yacht_path(@yacht.id)
+    else
+      render :new
+    end
   end
 
   private
@@ -22,6 +27,6 @@ class YachtsController < ApplicationController
   def yacht_params
     params.require(:yacht).permit(:title, :description, :weekly_price, :coordinates,
                                   :average_rating, :length, :number_of_crew, :number_of_guests,
-                                  :number_of_cabins, :beam, :cruising_speed, :build, :year)
+                                  :number_of_cabins, :beam, :cruising_speed, :build, :year, :user_id)
   end
 end

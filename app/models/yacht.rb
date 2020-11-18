82c1cde087c5c038c10663fb9a01b5f229c18663
donghyun kim <donghyunkim524@gmail.com>
2020-11-18 11:08:42 +0000

    add some validations and commented out

--- app/models/yacht.rb
+++ app/models/yacht.rb
@@ -7,4 +7,10 @@ class Yacht < ApplicationRecord
   # validates :description, length: { minimum: 20 }
   # validates :year, numericality: { only_integer: true }
   # validates :weekly_price, numericality: { only_integer: true }
+  # validates :length, numericality: { only_integer: true }
+  # validates :beam, numericality: { only_integer: true }
+  # validates :cruising_speed, numericality: { only_integer: true }
+  # validates :number_of_crew, numericality: { only_integer: true }
+  # validates :number_of_guests, numericality: { only_integer: true }
+  # validates :number_of_cabins, numericality: { only_integer: true }
 end

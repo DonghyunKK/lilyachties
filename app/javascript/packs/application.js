7e6c6a5ee61aa300d0517fa61cb70bff4af566c5
donghyun kim <donghyunkim524@gmail.com>
2020-11-20 14:16:38 +0000

    add flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,18 +22,18 @@ require("channels")
 // WRITE YOUR OWN JS STARTING FROM HERE 👇
 // ----------------------------------------------------
 
+
+
 // External imports
 import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { initMapbox } from '../plugins/init_mapbox';
+import { initFlatpickr } from "../plugins/flatpickr";
 
 document.addEventListener('turbolinks:load', () => {
   initMapbox();
+  initFlatpickr();
 })
 
-document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
-});
